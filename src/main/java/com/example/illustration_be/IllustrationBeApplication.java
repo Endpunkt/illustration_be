7a35d0630a2f00acb8213becb1dcafdf1e84af18
Helder Pereira <phelder@gmail.com>
2025-01-31 10:24:03 +0000

    First commit

---
+++ src/main/java/com/example/illustration_be/IllustrationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.illustration_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllustrationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllustrationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/illustration_be/IllustrationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.illustration_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllustrationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
